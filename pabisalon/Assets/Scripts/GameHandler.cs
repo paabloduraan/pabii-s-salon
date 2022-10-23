7758f80c1daf5bd6879c2f3a448c9cf78b19955b
Darmatage <JasonWiserArt@gmail.com>
2022-10-23 02:25:59 -0400

    Initial scoring system and round manager added

--- pabisalon/Assets/Scripts/GameHandler.cs
+++ pabisalon/Assets/Scripts/GameHandler.cs
@@ -66,22 +66,38 @@ public class GameHandler : MonoBehaviour {
             "I'm getting coffe with my ex"			
 			};
 	
+	//style variables
 	public static int currentHair = 0;
 	public static Color currentHairColor = new Color(0,0,0,1);
 	public static int currentHairColorID = 0;
+	public static int CurrentAccessory = 0;
 	
 	//score variables
+	public GameObject ScoreButton;
+	public GameObject GetEventButton;
+	public static bool hasStyled = false; 
 	public static int theScore = 0;
 	public static int theRound = 1;
+	public int maxRound = 5;
+	public static bool newRound = true;
 	public GameObject scoreDisplayText;
-	
+	public bool endGame = false;
 	
 	public void Start(){
 		DisplayPrompt();
-		if (currentHair > 0){
-			DisplayCurrentHair();
-		}
 		DisplayScore();
+		if (currentHair > 0){DisplayCurrentHair();}
+		if (newRound){
+			GetEventButton.SetActive(true);
+			ScoreButton.SetActive(false);
+			}
+		else {
+			GetEventButton.SetActive(false);
+			ScoreButton.SetActive(true);
+		}
+		hasHair = false;
+		hasColor = false;
+		hasAccessory = false;
 	}
 
 	public void Update(){
@@ -89,7 +105,7 @@ public class GameHandler : MonoBehaviour {
 		//if sceneName = styleme, if all 3 styles have been clicked, display done
 		if (SceneManager.GetActiveScene().name == "StyleRoom"){
 			//if ((hasHair)&&(hasColor)&&(hasAccessory)){
-			if ((hasHair)){
+			if ((hasHair)&&(hasColor)){
 				ButtonStyleDone.SetActive(true);
 			}
 			else{
@@ -97,6 +113,12 @@ public class GameHandler : MonoBehaviour {
 			}
 		}
 		
+		//endgame:
+		if (endGame){
+			//show final tally scripts go here
+			GetEventButton.SetActive(false);
+			ScoreButton.SetActive(false);
+		}
 	}
 
 
@@ -110,6 +132,8 @@ public class GameHandler : MonoBehaviour {
 	
 	//button function to get a new prompt
 	public void GimmePrompt(){
+		GetEventButton.SetActive(false);
+		newRound = false;
 		currentPromptNum = Random.Range(0, thePrompts.Length -1);
 		if ((currentPromptNum != PromptNum1)&&(currentPromptNum != PromptNum2)&&(currentPromptNum != PromptNum3)){
 			currentPrompt = thePrompts[currentPromptNum];
@@ -124,6 +148,7 @@ public class GameHandler : MonoBehaviour {
 	}
 
 	public void DisplayScore(){
+		if (theRound > maxRound) {theRound = maxRound; endGame=true;}
 		Text displayScoreTextTemp = scoreDisplayText.GetComponent<Text>();
         displayScoreTextTemp.text = "ROUND: " + theRound + ", SCORE: " + theScore;
 	}
@@ -180,9 +205,8 @@ public class GameHandler : MonoBehaviour {
 		else if (currentHair == 4){Straight();}
 		else if (currentHair == 5){Punk();}
 		else if (currentHair == 6){Mustache();}
-		else if (currentHair == 7){Puffs();}
-		else if (currentHair == 6){Natural();}
-		else if (currentHair == 7){Shaved();}
+		else if (currentHair == 7){Natural();}
+		else if (currentHair == 8){Shaved();}
 	}
 
 //hair style button functions
@@ -328,5 +352,316 @@ public class GameHandler : MonoBehaviour {
 			currentHairColorID = 6;
 			hasColor = true;
       } 
+	  
+	  
+	  //Scoring Round System for 27 options
+	  public void Scoring(){
+			GetEventButton.SetActive(true);
+			ScoreButton.SetActive(false);
+			newRound = true;
+			theRound += 1;
+		  
+		//dialogue #1, placeholder results
+		if (currentPromptNum == 0){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #2, placeholder results
+		if (currentPromptNum == 1){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}  
+		  
+		//dialogue #3, placeholder results
+		if (currentPromptNum == 2){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #4, placeholder results
+		if (currentPromptNum == 3){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		} 
+
+		  
+		//dialogue #5, placeholder results
+		if (currentPromptNum == 4){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #6, placeholder results
+		if (currentPromptNum == 5){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}  
+		  
+		//dialogue #7, placeholder results
+		if (currentPromptNum == 6){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #8, placeholder results
+		if (currentPromptNum == 7){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		} 
+
+		//dialogue #9, placeholder results
+		if (currentPromptNum == 8){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #10, placeholder results
+		if (currentPromptNum == 7){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}  
+		  
+		//dialogue #11, placeholder results
+		if (currentPromptNum == 10){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #12, placeholder results
+		if (currentPromptNum == 11){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		} 
+
+		  
+		//dialogue #13, placeholder results
+		if (currentPromptNum == 12){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #14, placeholder results
+		if (currentPromptNum == 13){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}  
+		  
+		//dialogue #15, placeholder results
+		if (currentPromptNum == 14){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #16, placeholder results
+		if (currentPromptNum == 15){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		} 
+
+		//dialogue #17, placeholder results
+		if (currentPromptNum == 16){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #18, placeholder results
+		if (currentPromptNum == 17){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}  
+		  
+		//dialogue #19, placeholder results
+		if (currentPromptNum == 18){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #20, placeholder results
+		if (currentPromptNum == 19){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		} 
+
+		  
+		//dialogue #21, placeholder results
+		if (currentPromptNum == 20){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #22, placeholder results
+		if (currentPromptNum == 21){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}  
+		  
+		//dialogue #23, placeholder results
+		if (currentPromptNum == 22){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #24, placeholder results
+		if (currentPromptNum == 23){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		} 
+
+		//dialogue #25, placeholder results
+		if (currentPromptNum == 24){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}  
+		  
+		//dialogue #26, placeholder results
+		if (currentPromptNum == 25){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		}
+		  
+		//dialogue #27, placeholder results
+		if (currentPromptNum == 26){
+			//hairtest
+			if ((currentHair == 1)||(currentHair == 3)){theScore +=1; Debug.Log("+1 for hair #" + currentHair);} else {theScore -=1;}
+			//colortest
+			if ((currentHairColorID == 3)){theScore +=1; Debug.Log("+1 for color #" + currentHairColorID);} else {theScore -=1;}
+			//accessory test
+			//if ((CurrentAccessory==1)||(CurrentAccessory==2)){theScore +=1; Debug.Log("+1 for accessory #" + CurrentAccessory);} else {theScore -=1;}
+			DisplayScore();
+		} 
+
+	  }
+	  
 	  
 }
